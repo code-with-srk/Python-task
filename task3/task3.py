ebeb36c317dddef694d173aebef93f6ae789010b
code-with-srk <surajkamble01997@gmail.com>
2024-11-16 15:41:25 +0530

    task details

---
+++ task3/task3.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int (input().strip())
+    
+
+
+def print_back_numbers():
+    n = int(input("Enter integer"))
+    for i in list(range(1,n+1)):
+        print(i,end='')
+
+print_back_numbers()
