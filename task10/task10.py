ba23d90db122a514c66d58786a34e47a3150b9c1
code-with-srk <surajkamble01997@gmail.com>
2024-11-28 16:22:17 +0530

    task details

--- task10/task10.py
+++ task10/task10.py
@@ -1,8 +1,8 @@
 def print_formatted(number):
-    width = len(bin(number)) - 2
     
+    width = len(bin(number)) - 2 
     for i in range(1, number + 1):
-        print(f"{i:{width}d} {i:{width}o} {i:{width}X} {i:{width}b}")
+           print(f"{i:{width}d} {i:{width}o} {i:{width}X} {i:{width}b}")
 
 if __name__ == '__main__':
     n = int(input())
