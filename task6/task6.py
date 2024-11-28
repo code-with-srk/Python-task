7a1efba5243f2a4c6f625bb825a2f271f31bcea3
code-with-srk <surajkamble01997@gmail.com>
2024-11-28 15:45:52 +0530

    task details

--- task6/task6.py
+++ task6/task6.py
@@ -1,8 +1,4 @@
-# Read the number of elements in the tuple
+
 n = int(input())
-
-# Read the space-separated integers and convert them to a tuple
 t = tuple(map(int, input().split()))
-
-# Compute and print the hash of the tuple
 print(hash(t))
