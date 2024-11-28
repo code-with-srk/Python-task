be279c983a78868eebb688f946c4c42cb74deae0
code-with-srk <surajkamble01997@gmail.com>
2024-11-28 15:34:08 +0530

    task details

--- task6/task6.py
+++ task6/task6.py
@@ -1,40 +1,8 @@
-if __name__ == '__main__':
-    n = int(input()) 
+# Read the number of elements in the tuple
+n = int(input())
 
-    {
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import math\n",
-    "import os\n",
-    "import random\n",
-    "import re\n",
-    "import sys\n",
-    "\n",
-    "\n",
-    "\n",
-    "if __name__ == '__main__':\n",
-    "    n = int(input().strip())\n",
-    "    if n%2 != 0:\n",
-    "        print(\"Weird\")\n",
-    "    elif n%2 == 0 and n>2 and n<=5:\n",
-    "        print(\"Not Weird\")\n",
-    "    elif n%2 ==0 and n > 6 and n <=20:\n",
-    "        print(\"Weird\")\n",
-    "    else:\n",
-    "        print(\"Not Weird\")\n"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+# Read the space-separated integers and convert them to a tuple
+t = tuple(map(int, input().split()))
+
+# Compute and print the hash of the tuple
+print(hash(t))
